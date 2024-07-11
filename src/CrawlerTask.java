fa35deebab90a64f00ee15a57479d8e0c9ba3ec0
Krishiv Gubba <kgubba@wisc.edu>
2024-07-11 23:38:50 +0530

    added jsoup jar file, modified webcrawler, started working on HTML Parser

--- src/CrawlerTask.java
+++ src/CrawlerTask.java
@@ -1,7 +1,54 @@
-public class CrawlerTask implements Runnable{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
+public class CrawlerTask implements Runnable{
+  private URLQueue queue;
+  public CrawlerTask(URLQueue queue){
+    this.queue = queue;
+  }
   @Override
   public void run() {
+    //should first deque from the queue
+    String target = queue.deque();
 
+  }
+
+  public static void main(String[] args) {
+    try {
+      URL url = new URL("https://en.wikipedia.org/wiki/Bromine");
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+
+      int responseCode = connection.getResponseCode();
+      if (responseCode == HttpURLConnection.HTTP_OK) { // success
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+            connection.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+          content.append(inputLine);
+        }
+        in.close();
+
+        // print result
+        ArrayList<String> res = HTMLParser.getText(content.toString());
+        for (String thing: res){
+          System.out.println(thing);
+        }
+      } else {
+        System.out.println("GET request failed");
+      }
+
+      connection.disconnect();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
--- src/HTMLParser.java
+++ src/HTMLParser.java
@@ -1,2 +1,28 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class HTMLParser {
+
+  public static ArrayList<String> getText(String HTML){
+    //converting the String into a document object
+    Document doc = Jsoup.parse(HTML);
+    Elements urls = doc.select("a[href]");
+    ArrayList<String> output = new ArrayList<>();
+    for (Element url : urls){
+      output.add(url.attr("href"));
+      System.out.println(url.attr("href"));
+    }
+    return output;
+  }
+
+  public static void main(String[] args) {
+    ArrayList<String> res = HTMLParser.getText("<html><body><a href='https://example.com'>Example" +
+        "</a></body></html>");
+    System.out.println(res);
+  }
 }
